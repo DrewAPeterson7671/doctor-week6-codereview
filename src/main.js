61b524491f1e20bc3f416c1ad46622e6e78c95b7
Student <example@example.com>
2019-11-22 10:40:40 -0800

    Open search to accept symptom input

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import './styles.css';
 import { DoctorService } from './../src/doctor-backend.js';
 
 $(document).ready(function(){
-  $('#symptominput').click(function() {
+  $('#symptominput').submit(function() {
     const symptom = $('#symptominput').val();
     $('#symptominput').val("");
 
     (async () => {
       let doctorService = new DoctorService();
-      const response = await movieService.getDoctorBySymptom(symptom);
+      const response = await doctorService.getDoctorBySymptom(symptom);
       getElements(response);
     })();
 
     function getElements(response) {
-      $('.showDoctor').text('${response.}')  /// NEEDS WORK
+      $('.showDoctor').text(`${response.data[0].profile.last_name}`);  /// NEEDS WORK
     }
   });
 
