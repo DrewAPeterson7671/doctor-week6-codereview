18e819c717ea5c0c28788d5c8fbb1fc2b4f96f77
Student <example@example.com>
2019-11-22 10:00:32 -0800

    begin fleshing out the backend of a async API call

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { DoctorService } from './../src/doctor-backend.js';
 
 $(document).ready(function(){
-  console.log("hello");
+  $('#symptominput').click(function() {
+    const symptom = $('#symptominput').val();
+    $('#symptominput').val("");
+
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await movieService.getDoctorBySymptom(symptom);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      $('.showDoctor').text('${response.}')  /// NEEDS WORK
+    }
+  });
 
 });
